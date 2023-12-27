b68d27bf5bbcbe3a360ec3b50d8fd124db29bf10
Ardatika <140362648+Ardatika@users.noreply.github.com>
2023-12-27 12:00:15 +0700

    Delete components directory

--- components/index.js
+++
@@ -1,7 +0,0 @@
-import Header from "./header";
-import CategoryButton from "./category-button";
-import Categories from "./categories";
-import NewsItem from "./news-item";
-
-export { Header, CategoryButton, Categories, NewsItem };
-
